449c92d992ece3af6bad5c020176aa8ab9a8b417
chencong <1145928494@qq.com>
2018-12-28 23:41:31 +0800

    .

--- 20181228/1.php
+++ 20181228/1.php
@@ -1,11 +1,11 @@
 <?php
- $arr = array(1=>'a',2=>'b',3=>'c',4=>'d');
+//  $arr = array(1=>'a',2=>'b',3=>'c',4=>'d');
 // for ($i=1; $i<=4 ;$i++) {
 //     echo "$arr[$i]<br>";
 // }  
-foreach ($arr as $k => $v) {
-    echo $v,'<br>';
-}
+// foreach ($arr as $k => $v) {
+//     echo $v,'<br>';  
+// }
 
 
 // $arr = array('name'=>'zs','age'=>'25','gender'=>'man');//关联数组
